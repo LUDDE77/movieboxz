cfd8d3441ff229e853eff03b60d9d994b1700867
LUDDE77 <martin.johan.ahl@gmail.com>
2026-01-26 11:31:15 +0100

    Add YouTube thumbnail fallback for broken poster URLs

--- iOS/MovieBoxZ/AppVersion.swift
+++ iOS/MovieBoxZ/AppVersion.swift
@@ -4,7 +4,7 @@ import Foundation
 /// INCREMENT THIS NUMBER EVERY TIME YOU UPDATE THE PROJECT
 struct AppVersion {
     static let current = "1.0.0"
-    static let build = 45
+    static let build = 46
 
     static var fullVersion: String {
         "\(current) (Build \(build))"
